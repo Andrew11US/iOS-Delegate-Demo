1cb99e51cfcb1bb4fd17c620fb4499902e4947ed
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-08-15 17:52:23 +0200

    Initial Commit

---
+++ Delegate Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate Demo
+//
+//  Created by Andrew on 8/15/20.
+//  Copyright © 2020 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
